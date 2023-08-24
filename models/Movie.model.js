b6d264aed5c63163343aac4dba0fc88546c9e9fd
MeenaThirunaukkarasu <134803165+MeenaThirunaukkarasu@users.noreply.github.com>
2023-08-24 23:23:18 +0200

    done

--- models/Movie.model.js
+++ models/Movie.model.js
@@ -1,17 +1,36 @@
-// models/Movie.model.js
+// // models/Movie.model.js
 
-const { Schema, model } = require('mongoose');
+// const { Schema, model } = require('mongoose');
 
-const movieSchema = new Schema(
+// const movieSchema = new Schema(
+//   {
+//     title: String,
+//     description: String,
+    
+//   },
+//   {
+//     timestamps: true
+//   }
+// );
+
+// module.exports = model('Movie', movieSchema);
+
+
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the User model to whatever makes sense in this case
+const moviesSchema = new Schema(
   {
     title: String,
-    description: String,
-    author: String,
-    rating: Number
-  },
-  {
-    timestamps: true
-  }
+    director: String,
+    stars: [String, String, String],
+    image: String,
+    description:String,
+    showtimes: [String, String, String]
+    },
+
 );
 
-module.exports = model('Movie', movieSchema);
+const Movie = model("Movie", moviesSchema);
+
+module.exports = Movie;
